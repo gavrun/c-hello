fd5b7bbfed0852dafa3b3a5edfb038ab584c99b7
gavrun <38539655+gavrun@users.noreply.github.com>
2025-09-20 19:46:11 +0100

    Hello

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World! It is C! \n");
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World! It is C++! \n";
+    return 0;
+}
